970419c76b5b676a7df2763f93dc119fa2741318
josephharveyangeles <josephharveyangeles@gmail.com>
2015-02-21 12:42:01 +0800

    created the first activity

---
+++ src/com/creosoft/ext/formsgen/Launcher_formsgen.java
@@ -0,0 +1,15 @@
+package com.creosoft.ext.formsgen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Launcher_formsgen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launcher_formsgen);
+	}
+}
