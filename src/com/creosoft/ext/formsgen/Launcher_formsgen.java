c85bafb7f945c97c37ac72ace218a388142107eb
josephharveyangeles <josephharveyangeles@gmail.com>
2015-02-21 17:38:06 +0800

    created the 'Hecate' library and made it work

--- src/com/creosoft/ext/formsgen/Launcher_formsgen.java
+++ src/com/creosoft/ext/formsgen/Launcher_formsgen.java
@@ -1,15 +1,17 @@
 package com.creosoft.ext.formsgen;
 
+import org.creosoft.hecate.utils.windowUtils.ScreenUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class Launcher_formsgen extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		ScreenUtils.forceFullScreen(this);
 		setContentView(R.layout.activity_launcher_formsgen);
 	}
 }
